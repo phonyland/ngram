2ad391ac8cd9a090bfa6d13bf5dd150f66bc8da5
Yunus Emre Deligöz <ye@deligoz.me>
2024-01-02 05:30:21 +0300

    Refactor test structures and add token filtering
    The 'TokenizerTest' method names are updated for a more concise explanation of the tests. Additionally, a new test case for token filtering rule was introduced. All the tests are organized using 'describe' and 'test' blocks instead of just 'it' for better readability and structure.

--- tests/TokenizerTest.php
+++ tests/TokenizerTest.php
@@ -5,132 +5,138 @@ declare(strict_types=1);
 use Phonyland\NGram\Tokenizer;
 use Phonyland\NGram\TokenizerFilterType;
 
-it('Tokenizer@tokenize: Seperates the text with the given separator', function (): void {
-    // Arrange
-    $tokenizer = new Tokenizer();
-    $tokenizer->addWordSeparatorPattern(TokenizerFilterType::WHITESPACE_SEPARATOR);
+describe('Tokenizer@tokenize', function () {
+    test('it can separate the text from the given separator', function (): void {
+        // Arrange
+        $tokenizer = new Tokenizer();
+        $tokenizer->addWordSeparatorPattern(wordSeparationPattern: TokenizerFilterType::WHITESPACE_SEPARATOR);
 
-    $text = 'sample text';
+        $text = 'sample text';
 
-    // Act
-    $result = $tokenizer->tokenize($text);
+        // Act
+        $result = $tokenizer->tokenize($text);
 
-    // Assert
-    expect($result)->toBe(['sample', 'text']);
+        // Assert
+        expect($result)->toBe(['sample', 'text']);
+    });
+
+    test('it can separate the text from multiple separators', function (): void {
+        // Arrange
+        $tokenizer = new Tokenizer();
+        $tokenizer
+            ->addWordSeparatorPattern(' ')
+            ->addWordSeparatorPattern(';');
+
+        $text = 'sample text;sample;text';
+
+        // Act
+        $result = $tokenizer->tokenize($text);
+
+        // Assert
+        expect($result)->toBe(['sample', 'text', 'sample', 'text']);
+    });
+
+    test('it can separate the text from regex patterns', function (): void {
+        // Arrange
+        $tokenizer = new Tokenizer();
+        $tokenizer->addWordSeparatorPattern('\s');
+
+        $text = 'sample     text '.PHP_EOL.'sample text';
+
+        // Act
+        $result = $tokenizer->tokenize($text);
+
+        // Assert
+        expect($result)->toBe(['sample', 'text', 'sample', 'text']);
+    });
+
+    test('A minimum word length can be set', function (): void {
+        // Arrange
+        $tokenizer = new Tokenizer();
+        $tokenizer->addWordSeparatorPattern(TokenizerFilterType::WHITESPACE_SEPARATOR);
+
+        $text = 'A sample text with some meaningless words';
+
+        // Act
+        $result = $tokenizer->tokenize(text: $text, minWordLength: 6);
+
+        // Assert
+        expect($result)->toBe(['sample', 'meaningless']);
+    });
+
+    it('it can filter the tokens by given removal rule', function (): void {
+        // Arrange
+        $tokenizer = (new Tokenizer())
+            ->addWordSeparatorPattern(TokenizerFilterType::WHITESPACE_SEPARATOR);
+
+        // Act I
+        $tokenizer->addWordFilterRule('/m/');
+        // Assert I
+        expect($tokenizer->tokenize('sample text'))->toBe(['saple', 'text']);
+
+        // Act II
+        $tokenizer->addWordFilterRule('/x/', 'q');
+        // Assert II
+        expect($tokenizer->tokenize('sample text'))->toBe(['saple', 'teqt']);
+    });
 });
 
-it('Tokenizer@tokenize: Seperates the text with multiple separators', function (): void {
-    // Arrange
-    $tokenizer = new Tokenizer();
-    $tokenizer
-        ->addWordSeparatorPattern(' ')
-        ->addWordSeparatorPattern(';');
+describe('Tokenizer@sentences', function () {
+    test('it can separate the text with the given punctuation into sentences', function (): void {
+        // Act
+        $tokenizer = new Tokenizer();
+        $tokenizer
+            ->addSentenceSeparatorPattern('.')
+            ->addSentenceSeparatorPattern('!')
+            ->addSentenceSeparatorPattern('?');
 
-    $text = 'sample text;sample;text';
+        $text = 'Sample Sentence. Sample Sentence! Sample Sentence? Sample Sentence no. 4?! Sample sample sentence... End';
 
-    // Act
-    $result = $tokenizer->tokenize($text);
+        // Act
+        $result = $tokenizer->sentences($text);
 
-    // Assert
-    expect($result)->toBe(['sample', 'text', 'sample', 'text']);
+        // Assert
+        expect($result)->toBe([
+            'Sample Sentence.',
+            'Sample Sentence!',
+            'Sample Sentence?',
+            'Sample Sentence no.',
+            '4?!',
+            'Sample sample sentence...',
+            'End',
+        ]);
+    });
 });
 
-it('Tokenizer@tokenize: Seperates the text with regex patterns', function (): void {
-    // Arrange
-    $tokenizer = new Tokenizer();
-    $tokenizer->addWordSeparatorPattern('\s');
+describe('Tokenizer@tokenizeBySentences', function () {
+    test('it can separate the text into tokens by sentences', function (): void {
+        // Arrange
+        $tokenizer = new Tokenizer();
+        $tokenizer
+            ->addSentenceSeparatorPattern('.')
+            ->addSentenceSeparatorPattern('!')
+            ->addSentenceSeparatorPattern('?')
+            ->addWordFilterRule(TokenizerFilterType::NO_SYMBOLS)
+            ->addWordSeparatorPattern(TokenizerFilterType::WHITESPACE_SEPARATOR);
 
-    $text = 'sample     text '.PHP_EOL.'sample text';
+        $text = 'Sample Sentence. Sample Sentence! Sample Sentence? Sample Sentence no. 4?! Sample sample sentence... End';
 
-    // Act
-    $result = $tokenizer->tokenize($text);
+        // Act
+        $result = $tokenizer->tokenizeBySentences($text);
 
-    // Assert
-    expect($result)->toBe(['sample', 'text', 'sample', 'text']);
+        // Assert
+        expect($result)->toBe([
+            ['Sample', 'Sentence'],
+            ['Sample', 'Sentence'],
+            ['Sample', 'Sentence'],
+            ['Sample', 'Sentence', 'no'],
+            ['Sample', 'sample', 'sentence'],
+            ['End'],
+        ]);
+    });
 });
 
-it('Tokenizer@tokenize: A minimum word length can be set', function (): void {
-    // Arrange
-    $tokenizer = new Tokenizer();
-    $tokenizer->addWordSeparatorPattern(TokenizerFilterType::WHITESPACE_SEPARATOR);
-
-    $text = 'A sample text with a some meaningless words';
-
-    // Act
-    $result = $tokenizer->tokenize($text, 6);
-
-    // Assert
-    expect($result)->toBe(['sample', 'meaningless']);
-});
-
-it('Tokenizer@sentences: Separates the text with the given punctuation into sentences', function (): void {
-    // Act
-    $tokenizer = new Tokenizer();
-    $tokenizer
-        ->addSentenceSeparatorPattern('.')
-        ->addSentenceSeparatorPattern('!')
-        ->addSentenceSeparatorPattern('?');
-
-    $text = 'Sample Sentence. Sample Sentence! Sample Sentence? Sample Sentence no. 4?! Sample sample sentence... End';
-
-    // Act
-    $result = $tokenizer->sentences($text);
-
-    // Assert
-    expect($result)->toBe([
-        'Sample Sentence.',
-        'Sample Sentence!',
-        'Sample Sentence?',
-        'Sample Sentence no.',
-        '4?!',
-        'Sample sample sentence...',
-        'End',
-    ]);
-});
-
-it('Tokenizer@tokenizeBySentences: Separates the text into tokens by sentences', function (): void {
-    // Arrange
-    $tokenizer = new Tokenizer();
-    $tokenizer
-        ->addSentenceSeparatorPattern('.')
-        ->addSentenceSeparatorPattern('!')
-        ->addSentenceSeparatorPattern('?')
-        ->addWordFilterRule(TokenizerFilterType::NO_SYMBOLS)
-        ->addWordSeparatorPattern(TokenizerFilterType::WHITESPACE_SEPARATOR);
-
-    $text = 'Sample Sentence. Sample Sentence! Sample Sentence? Sample Sentence no. 4?! Sample sample sentence... End';
-
-    // Act
-    $result = $tokenizer->tokenizeBySentences($text);
-
-    // Assert
-    expect($result)->toBe([
-        ['Sample', 'Sentence'],
-        ['Sample', 'Sentence'],
-        ['Sample', 'Sentence'],
-        ['Sample', 'Sentence', 'no'],
-        ['Sample', 'sample', 'sentence'],
-        ['End'],
-    ]);
-});
-
-it('Tokenizer@tokenize: Filters the tokens by given removal rule', function (): void {
-    // Arrange
-    $tokenizer = (new Tokenizer())
-        ->addWordSeparatorPattern(TokenizerFilterType::WHITESPACE_SEPARATOR);
-
-    // Act I
-    $tokenizer->addWordFilterRule('/m/');
-    // Assert I
-    expect($tokenizer->tokenize('sample text'))->toBe(['saple', 'text']);
-
-    // Act II
-    $tokenizer->addWordFilterRule('/x/', 'q');
-    // Assert II
-    expect($tokenizer->tokenize('sample text'))->toBe(['saple', 'teqt']);
-});
-
-it('Tokenizer: Can convert tokens to lowercase', function (): void {
+test('it can convert tokens to lowercase', function (): void {
     // Arrange
     $tokenizer = (new Tokenizer())
         ->addWordSeparatorPattern(TokenizerFilterType::WHITESPACE_SEPARATOR)
@@ -145,7 +151,7 @@ it('Tokenizer: Can convert tokens to lowercase', function (): void {
     expect($result)->toBe(['sample', 'text']);
 });
 
-test('Tokenizer: Can be converted to an array', function (): void {
+test('it can convert tokens to an array', function (): void {
     // Arrange
     $tokenizer = (new Tokenizer())
         ->addWordFilterRule(TokenizerFilterType::NO_SYMBOLS)
